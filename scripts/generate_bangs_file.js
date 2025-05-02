b76e0dd188edbae831b2fb96c08f295578bef6ce
Yaroslav Pietukhov <77742707+valsoray-dev@users.noreply.github.com>
2025-05-02 16:46:49 +0300

    diff in bangs.ts is more visual now

--- scripts/generate_bangs_file.js
+++ scripts/generate_bangs_file.js
@@ -6,9 +6,9 @@ const bangsTsPath = path.resolve(process.cwd(), "src", "bangs.ts");
 
 const bangsJson = JSON.parse(fs.readFileSync(bangJsPath, "utf8"));
 const entries = bangsJson
-	.map((bang) => `["${bang.t}",${JSON.stringify(bang.u)}]`)
-	.join(",");
+  .map((bang) => `\t["${bang.t}", ${JSON.stringify(bang.u)}]`)
+  .join(",\n");
 
-const content = `export const BANGS = new Map<string, string>([${entries}]);\n`;
+const content = `export const BANGS = new Map<string, string>([\n${entries}\n]);\n`;
 fs.writeFileSync(bangsTsPath, content, "utf8");
 console.log(`Successfully generated bangs file at ${bangsTsPath}`);
